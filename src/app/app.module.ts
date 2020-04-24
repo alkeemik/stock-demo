17807d9ab1271b7497ac32f7571d00adc96c555d
Heinar Ojala <heinar@skyselect.com>
2020-04-24 17:34:35 +0300

    Brainbase homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { StockListComponent } from './stock-list/stock-list.component';
+import { StockDemoComponent } from './stock-demo/stock-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockListComponent,
+    StockDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
